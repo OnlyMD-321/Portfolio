dec1cbb99d1ab0661995a668780ff6d68b51d4bf
Only MD <92043557+OnlyMD-321@users.noreply.github.com>
2025-11-27 14:29:56 +0100

    Add Master's Degree entry and update IDs
    Added a new education entry for a Master's Degree and updated IDs for existing entries.

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,18 +1,24 @@
 export const educations = [
+    {
+    id: 1,
+    title: "Master's Degree in Full-Stack Development",
+    duration: "2025 - Current",
+    institution: "Maroc Ynov Campus",
+  },
   {
-    id: 1, 
+    id: 2, 
     title: "Bachelor 3 in Software Engineering",
     duration: "2024 - 2025",
     institution: "Maroc Ynov Campus",
   },
   {
-    id: 2,
+    id: 3,
     title: "Qulification Certificate Mern-Stack",
     duration: "2023 - 2024",
     institution: "JobInTech/Ynov Campus",
   },
   {
-    id: 3,
+    id: 4,
     title: "Specialised Technicien diploma",
     duration: "2021 - 2023",
     institution: "ISGI Casablanca",
