9cfba0f3d30b9a9a37ce3f6c4c8c9ee6a3e507a5
Mouad Omlil <mouadom2003@gmail.com>
2026-04-10 13:28:25 +0100

    Display profile photo in strict square frame

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -23,13 +23,15 @@ function AboutSection() {
           </p>
         </div>
         <div className="flex justify-center order-1 lg:order-2">
-          <Image
-            src={personalData.profile}
-            width={280}
-            height={280}
-            alt="Abu Said"
-            className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
-          />
+          <div className="w-[280px] h-[280px] overflow-hidden rounded-lg">
+            <Image
+              src={personalData.profile}
+              width={280}
+              height={280}
+              alt="OMLIL Mouad"
+              className="w-full h-full object-cover object-center transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
+            />
+          </div>
         </div>
       </div>
     </div>
